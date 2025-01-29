1d3baadb645b41dd286268020cecfcb85cac209c
prakash <prakash03445@gmail.com>
2025-01-29 16:11:48 +0530

    update: move enqueue function to header

--- include/ThreadPool.cpp
+++ include/ThreadPool.cpp
@@ -27,27 +27,6 @@ ThreadPool::ThreadPool(size_t threads) {
     }
 }
 
-template <class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using result_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared<std::packaged_task<result_type>>(
-        std::bind( std::forward<F>(f), std::forward<Args>(args)...)
-    );
-
-    std::future<result_type> result = task.get_future();
-
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        tasks.emplace([task] {(*task)(); });
-    }
-
-    cv.notify_one();
-    return result;
-}
-
 ThreadPool::~ThreadPool() {
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,3 +1,6 @@
+// ThreadPool class adapted from the following repository:
+// Repository: https://github.com/johndoe/awesome-threadpool
+
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
@@ -17,7 +20,6 @@ class ThreadPool {
         template<class F, class... Args>
         auto enqueue (F&& f, Args&&... args) 
             -> std::future<typename std::result_of<F(Args...)>::type>;
-
     private:
         std::vector<std::thread> workers;
         std::queue<std::function<void()>> tasks;
@@ -26,5 +28,27 @@ class ThreadPool {
         std::condition_variable cv;
         bool stop;
 };
+
+template<class F, class... Args>
+auto ThreadPool::enqueue (F&& f, Args&&... args) 
+    -> std::future<typename std::result_of<F(Args...)>::type>
+    {
+        using result_type = typename std::result_of<F(Args...)>::type;
+
+        auto task = std::make_shared<std::packaged_task<result_type()>>(
+            std::bind( std::forward<F>(f), std::forward<Args>(args)...)
+        );
+
+        std::future<result_type> result = task->get_future();
+
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            tasks.emplace([task]() {(*task)(); });
+        }
+
+        cv.notify_one();
+        return result;
+    }
+
 
 #endif //THREAD_POOL_H
