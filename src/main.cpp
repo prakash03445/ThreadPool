97d70fb56269e7df5ad04420d335abd541286855
prakash <prakash03445@gmail.com>
2025-01-29 16:19:43 +0530

    Test: testing the threadpool class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include "ThreadPool.h"
+#include <iostream>
 
 int main() {
+    ThreadPool pool(4);
+
+    std::vector<std::future<int>> results;
+
+    for (int i = 0; i < 20; ++i) {
+        results.emplace_back(
+            pool.enqueue([i] {
+                return i *  10;
+            })
+        );
+    }
+
+    for (auto& result: results) {
+        std::cout << result.get() << std::endl;
+    }
+
     return 0;
 }
